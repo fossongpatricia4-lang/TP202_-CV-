acd9a44c54c9120766a65821cd36443833b6f375
patricia247git config --global user.email fossongpatricia@gmail.com <fossongpatricia4@gmail.com>
2026-05-08 13:23:29 +0200

    Matricule

--- Fossong-Patricia/data.js
+++ Fossong-Patricia/data.js
@@ -18,7 +18,9 @@ const MEMBER = {
   name:      "FOSSONG TSOFACK PATRICIA",           // Ex: "Alice Mbarga"
   name_en:   "FOSSONG TSOFACK PATRICIA",           // English version
   role:      "Développeuse Front-end et UI/UX Designer",   // Ex: "Développeuse Front-end"
-  role_en:   "Front-end Developer and UI/UX Designer",     // English
+  role_en:   "Front-end Developer and UI/UX Designer",  
+  matricule: "24G2975", 
+  matricule_en: "24G2975",   // English
   specialty: "Génie Logiciel",  // Ex: "HTML / CSS / JavaScript"
   specialty_en: "Software Engineering", // English
   city:      "Yaoundé",                 // Ex: "Yaoundé"
